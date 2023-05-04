04d98c78dd9bef36777414d9b87a9fd2113c37d0
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-05-04 20:24:34 +0300

    add welcomeVC

---
+++ AboutMeApp/WelcomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WelcomeViewController.swift
+//  AboutMeApp
+//
+//  Created by Goodwasp on 04.05.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
